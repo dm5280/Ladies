7f95a73a3aab42f886e1fcda02c0e1c2837ef033
Dmitry <didib5280@gmail.com>
2022-03-30 10:10:33 -0400

    Update

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
     app_name: 'Cool Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
-    collection_file_name: 'ladybulls1.json',
+    collection_file_name: 'ladybulls.json',
     collection_contract_address: '0xcb8695bec262679b942446cd94dfF849c76753c9',
     collection_name: '10000 ENFT Bull Club Lady Bulls',
     collection_description: '10000 ENFT Bull Club Lady Bulls',
